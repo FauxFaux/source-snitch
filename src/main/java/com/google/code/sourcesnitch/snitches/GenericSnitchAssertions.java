4ada70682dc5d8704b817e6f29c3c17e57d462ef
Nick Faulkner <nickbfaulkner@gmail.com>
2011-04-19 00:45:27 +0100

    comments

--- src/main/java/com/google/code/sourcesnitch/snitches/GenericSnitchAssertions.java
+++ src/main/java/com/google/code/sourcesnitch/snitches/GenericSnitchAssertions.java
@@ -20,11 +20,17 @@ public interface GenericSnitchAssertions {
     
     /**
      * Throws if any file does not match passed pattern.
+     *
+     * Matches are made using single-line mode
+     * (I.E. the period character matches line terminators).
      */
     void match(String pattern) throws SnitchAssertionFailure;
 
     /**
      * Throws if any file matches the passed pattern.
+     * 
+     * Matches are made using single-line mode
+     * (I.E. the period character matches line terminators).
      */
     void doNotMatch(String pattern) throws SnitchAssertionFailure;
 }
